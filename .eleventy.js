b285028c24dfe19697907e6d58050d8c2ad9e5d0
cybedra <147957633+cybedra@users.noreply.github.com>
2025-04-29 17:09:52 -0700

    updated some css

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/main.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
 
   return {
     markdownTemplateEngine: 'njk',
